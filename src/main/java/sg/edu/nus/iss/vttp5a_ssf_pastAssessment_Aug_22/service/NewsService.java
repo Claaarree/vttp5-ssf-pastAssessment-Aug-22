db4fa08fe66ccc21565311ec624e1388274a8600
Claaarree <clare.fh@hotmail.com>
2024-12-10 17:44:32 +0800

    resolved some warnings

--- src/main/java/sg/edu/nus/iss/vttp5a_ssf_pastAssessment_Aug_22/service/NewsService.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_pastAssessment_Aug_22/service/NewsService.java
@@ -4,7 +4,6 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
